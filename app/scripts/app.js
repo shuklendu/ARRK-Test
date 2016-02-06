b1b554d43c0c5e6b4b16e7b847edaa2cb912b1fd
shuklendu <shuklendu.ha@gmail.com>
2016-02-06 02:13:55 -0700

    Created Repository
    Created Repository

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name yoApp
+ * @description
+ * # yoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('arkApp', [])
+  .config(function () {
+
+  });
---
+++ app/scripts/controllers/maincontroller.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name arkApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the arkApp
+ */
+angular.module('arkApp')
+  .controller('MainCtrl', function ($scope) {
+   $scope.isActivityOpen = true;
+});
